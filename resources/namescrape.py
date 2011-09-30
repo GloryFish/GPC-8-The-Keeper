6609f33a3880c308cacc0d376f5ce89f93d7ecba
Jay Roberts <jay@gloryfish.org>
2011-09-29 23:00:08 -0400

    Add female names as well

--- resources/namescrape.py
+++ resources/namescrape.py
@@ -3,19 +3,34 @@
 import BeautifulSoup
 import urllib2
 
-print 'Loading site...'
+print 'Loading male names...'
 
-output = open('male-names.txt', 'w')
+output_male = open('male-names.txt', 'w')
 
 html = urllib2.urlopen('http://names.mongabay.com/male_names_alpha.htm')
 soup = BeautifulSoup.BeautifulSoup(html)
 items = soup.findAll('table', style='table1')[0].findAll('tr')
 
-print 'Writing names...'
+print 'Writing male names...'
 
 for item in items[1:]:
-    output.write(item.td.text.title() + "\n")
+    output_male.write(item.td.text.title() + "\n")
 
-output.close()
+output_male.close()
+
+print 'Loading female names... '
+
+output_female = open('female-names.txt', 'w')
+
+html = urllib2.urlopen('http://names.mongabay.com/female_names_alpha.htm')
+soup = BeautifulSoup.BeautifulSoup(html)
+items = soup.findAll('table', style='table1')[0].findAll('tr')
+
+print 'Writing female names...'
+
+for item in items[1:]:
+    output_female.write(item.td.text.title() + "\n")
+
+output_female.close()
 
 print 'Done.'

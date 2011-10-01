e8047b3d79742d0a34f79cde159a11f284d1338d
Jay Roberts <jay@gloryfish.org>
2011-10-01 13:56:20 -0400

    Add Mob stub.

---
+++ TheKeeper/TheKeeper/Mob.cpp
@@ -0,0 +1,9 @@
+//
+//  Mob.cpp
+//  TheKeeper
+//
+//  Created by Jay Roberts on 10/1/11.
+//  Copyright 2011 GloryFish.org. All rights reserved.
+//
+
+#include <iostream>
---
+++ TheKeeper/TheKeeper/Mob.hpp
@@ -0,0 +1,30 @@
+//
+//  Mob.hpp
+//  TheKeeper
+//
+//  Created by Jay Roberts on 10/1/11.
+//  Copyright 2011 GloryFish.org. All rights reserved.
+//
+
+#ifndef TheKeeper_Mob_hpp
+#define TheKeeper_Mob_hpp
+
+#include "GFE/Entity.hpp"
+
+namespace Keeper {
+    
+    class Mob : GFE::Entity {
+    public:
+        
+        
+        
+    private:
+        
+        
+        
+    }
+    
+    
+}
+
+#endif
